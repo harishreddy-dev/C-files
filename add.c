b722537d0c58a5400897b4be12fbc88e0d2ac390
harishreddy-dev <harishreddy0269@gmail.com>
2025-11-24 20:16:56 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter any two numbers\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("the sum of two numbers = %d",sum);
+    return 0;
+}
